b569a0faf8ec1b53389a6de969d4b4fe682349b6
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-09-18 11:48:46 -0400

    Added a prompt to my greeting program

--- hello.rb
+++ hello.rb
@@ -1,6 +1,8 @@
-name = ARGV[0] if ARGV[0]
-loop do
-  name = STDIN.gets.chomp if name.nil?
-  puts "Hello, #{name}!"
-  name = nil
+if ARGV[0]
+  name = ARGV[0]
+else
+  puts "What is your name?"
+  name = STDIN.gets.chomp
 end
+
+puts "Hello, #{name}!"
